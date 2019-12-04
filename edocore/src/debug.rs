f60e353dd60875711e8d44db1cceb4d670dded8f
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-12-04 22:29:24 +0100

    fixed debug::log

--- edocore/src/debug.rs
+++ edocore/src/debug.rs
@@ -5,8 +5,11 @@
 // Created by Victor on 2019/10/24
 //=============================================================================
 
+use log::{debug, error, info, trace, warn};
+
 pub fn log(s: &str) {
-    println!("{}", s); // TODO: We should log to a file, etc
+    //println!("{}", s); // TODO: We should log to a file, etc
+    info!("{}", s);
 }
 
 pub enum LogType {
